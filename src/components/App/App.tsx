598199b6fc2a6a0deda1ce8b1a0d232711afdc15
Andrew_Krysenko <krysenko92@gmail.com>
2025-09-24 23:19:48 +0200

    react-query4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["movies", searchQuery, page],
     queryFn: () =>
       fetchMovies(
@@ -50,13 +50,7 @@ export default function App() {
       <Toaster position="top-right" />
 
       {isLoading && <Loader />}
-      {isError && (
-        <>
-          {console.log(error)}
-          <ErrorMessage />
-        </>
-      )}
-
+      {isError && <ErrorMessage />}
       {!isLoading && !isError && (
         <>
           <MovieGrid movies={moviesData} onSelect={handleSelectMovie} />
